0ca36a0569b9cfebb61719db3bc534542794a729
Deibit <doliva@refineriaweb.com>
2016-11-13 16:51:20 +0100

    Added requests

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 from User import User
@@ -23,13 +23,26 @@ def users_list():
     return jsonify(response)
 
 @app.route("/expenses")
-def expenses_list():
+def expenses():
+    if 'POST' == request.method:
+        response = new_expense()
+
+    if 'GET' == request.method:
+        response = list_expenses()
+
+    return jsonify(response)
+
+def list_expenses():
     expenses = Expense.query.all()
     response = []
     for expense in expenses:
         response.append(expense.serialize())
 
-    return jsonify(response)
+    return response
+
+def new_expense():
+    return 'yolo'
+
 
 db.init_app(app)
 if __name__ == "__main__":
